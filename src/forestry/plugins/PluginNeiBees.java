4af8d26a82bcabf1dd344acabf7b6dcf5a460c3f
bdew <blackdew@gmail.com>
2013-05-10 20:58:50 +0300

    Lets try a 1.4.7 backport

--- src/forestry/plugins/PluginNeiBees.java
+++ src/forestry/plugins/PluginNeiBees.java
@@ -9,8 +9,18 @@
 
 package forestry.plugins;
 
+import java.util.Random;
+
+import cpw.mods.fml.common.network.IGuiHandler;
 import net.bdew.neibees.NeiBees;
+import net.minecraft.command.ICommand;
+import net.minecraft.world.World;
+import forestry.api.core.IOreDictionaryHandler;
+import forestry.api.core.IPacketHandler;
+import forestry.api.core.IPickupHandler;
 import forestry.api.core.IPlugin;
+import forestry.api.core.IResupplyHandler;
+import forestry.api.core.ISaveEventHandler;
 import forestry.api.core.PluginInfo;
 
 @PluginInfo(name = "NEI Bees Plugin", pluginID = "neibees", version = "@@VERSION@@")
@@ -30,5 +40,59 @@ public class PluginNeiBees implements IPlugin {
     }
 
     public static void modLoaded() {
+    }
+
+    @Override
+    public String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void generateSurface(World world, Random rand, int chunkX, int chunkZ) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public IGuiHandler getGuiHandler() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IPacketHandler getPacketHandler() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IPickupHandler getPickupHandler() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IResupplyHandler getResupplyHandler() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ISaveEventHandler getSaveEventHandler() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public IOreDictionaryHandler getDictionaryHandler() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ICommand[] getConsoleCommands() {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
