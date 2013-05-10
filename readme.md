250f8b644fd370e7461674039998a5a0273d0bb5
bdew <blackdew@gmail.com>
2013-05-10 17:44:18 +0300

    Added copyright notice and license

--- readme.md
+++ readme.md
@@ -7,3 +7,8 @@
 ![Bee Breeding window](http://i.imgur.com/ENCP9He.png)
 
 ![Bee Products window](http://i.imgur.com/Jwv4n0Q.png)
+
+***
+
+This mod is distributed under the terms of the Minecraft Mod Public License 1.0, or MMPL. 
+Please check the contents of the license located in https://raw.github.com/bdew/neibees/master/MMPL-1.0.txt
--- src/net/bdew/neibees/Utils.java
+++ src/net/bdew/neibees/Utils.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) bdew, 2013
+ * https://github.com/bdew/neibees
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * https://raw.github.com/bdew/neibees/master/MMPL-1.0.txt
+ */
+
 package net.bdew.neibees;
 
 import net.minecraft.client.Minecraft;
