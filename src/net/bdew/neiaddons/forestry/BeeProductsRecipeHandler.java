6e7fb1de56ba1990e27ff687e53fe59e5e30a78a
bdew <blackdew@gmail.com>
2013-05-20 01:28:25 +0300

    fix copyrights

--- src/net/bdew/neiaddons/forestry/BeeProductsRecipeHandler.java
+++ src/net/bdew/neiaddons/forestry/BeeProductsRecipeHandler.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (c) bdew, 2013
- * https://github.com/bdew/neibees
+ * https://github.com/bdew/neiaddons
  *
  * This mod is distributed under the terms of the Minecraft Mod Public
  * License 1.0, or MMPL. Please check the contents of the license located in
- * https://raw.github.com/bdew/neibees/master/MMPL-1.0.txt
+ * https://raw.github.com/bdew/neiaddons/master/MMPL-1.0.txt
  */
 
 package net.bdew.neiaddons.forestry;
