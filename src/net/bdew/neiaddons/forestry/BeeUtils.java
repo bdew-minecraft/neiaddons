31ab1f4dfe88333d6353022ed7edbda996897909
bdew <blackdew@gmail.com>
2013-05-25 10:26:06 +0300

    added stackFromSecies - will replace stackFromAllele and work with bees/trees/etc.

--- src/net/bdew/neiaddons/forestry/BeeUtils.java
+++ src/net/bdew/neiaddons/forestry/BeeUtils.java
@@ -11,27 +11,66 @@ package net.bdew.neiaddons.forestry;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import forestry.api.apiculture.EnumBeeType;
 import forestry.api.apiculture.IAlleleBeeSpecies;
-import forestry.api.apiculture.IBee;
-import forestry.api.apiculture.IBeeGenome;
+import forestry.api.apiculture.IBeeRoot;
+import forestry.api.arboriculture.EnumGermlingType;
+import forestry.api.arboriculture.ITreeRoot;
 import forestry.api.genetics.AlleleManager;
 import forestry.api.genetics.IAllele;
 import forestry.api.genetics.IAlleleSpecies;
+import forestry.api.genetics.IIndividual;
+import forestry.api.genetics.ISpeciesRoot;
 
 public class BeeUtils {
+    public enum RecipePosition {
+        Parent1, Parent2, Offspring;
+    }
+    
+    public static Map<RecipePosition,Integer> beePositionToType;
+    public static Map<RecipePosition,Integer> treePositionToType;
+    
+    static {
+        beePositionToType = new HashMap<RecipePosition, Integer>();
+        beePositionToType.put(RecipePosition.Parent1, EnumBeeType.PRINCESS.ordinal());
+        beePositionToType.put(RecipePosition.Parent2, EnumBeeType.DRONE.ordinal());
+        beePositionToType.put(RecipePosition.Offspring, EnumBeeType.QUEEN.ordinal());
+
+        beePositionToType = new HashMap<RecipePosition, Integer>();
+        beePositionToType.put(RecipePosition.Parent1, EnumGermlingType.POLLEN.ordinal());
+        beePositionToType.put(RecipePosition.Parent2, EnumGermlingType.GERMLING.ordinal());
+        beePositionToType.put(RecipePosition.Offspring, EnumGermlingType.SAPLING.ordinal());
+    }
+
+    public static ItemStack stackFromSecies(IAlleleSpecies species, RecipePosition position) {
+        ISpeciesRoot root = species.getRoot();
+        int type = 0;
+        if (root instanceof IBeeRoot) {
+            type = beePositionToType.get(position);
+        } else if (root instanceof ITreeRoot) {
+            type = beePositionToType.get(position);
+        }
+        return stackFromSecies(species, type);
+    }
+
+    public static ItemStack stackFromSecies(IAlleleSpecies species, int type) {
+        ISpeciesRoot root = species.getRoot();
+        IAllele[] template = root.getTemplate(species.getUID());
+        IIndividual individual = root.templateAsIndividual(template);
+        individual.analyze();
+        return root.getMemberStack(individual, type);
+    }
+
+    
+    @Deprecated
     public static ItemStack stackFromAllele(IAllele allele, EnumBeeType type) {
         assert allele instanceof IAlleleSpecies;
-        IAlleleSpecies species = (IAlleleSpecies) allele;
-        IAllele[] template = AddonForestry.beeRoot.getTemplate(species.getUID());
-        IBeeGenome genome = AddonForestry.beeRoot.templateAsGenome(template);
-        IBee bee = AddonForestry.beeRoot.getBee(Minecraft.getMinecraft().theWorld, genome);
-        bee.analyze();
-        return AddonForestry.beeRoot.getMemberStack(bee, type.ordinal());
+        return stackFromSecies((IAlleleSpecies)allele, type.ordinal());
     }
     
     public static Collection<IAlleleBeeSpecies> getAllBeeSpecies(boolean includeBlacklisted) {
