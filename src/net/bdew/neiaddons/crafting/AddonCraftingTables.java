17e7c6691224573db96c7d510ac2ff6944391601
Ryan Liptak <squeek502@hotmail.com>
2015-01-05 12:46:49 -0800

    Remove outdated/broken TiC crafting station compatibility
     * See SlimeKnights/TinkersConstruct#1288

--- src/net/bdew/neiaddons/crafting/AddonCraftingTables.java
+++ src/net/bdew/neiaddons/crafting/AddonCraftingTables.java
@@ -62,7 +62,6 @@ public class AddonCraftingTables extends BaseAddon {
         tryLoadTableClass("EE3", "com.pahimar.ee3.client.gui.inventory.GuiPortableCrafting", "EE3 Minium Stone");
         tryLoadTableClass("BuildCraft|Factory", "buildcraft.factory.gui.GuiAutoCrafting", "BC Autorcrafting Table");
         tryLoadTableClass("powersuitaddons", "andrew.powersuits.client.PortableCraftingGui", "MPSA In-Place Assembler");
-        tryLoadTableClass("TConstruct", "tconstruct.tools.gui.CraftingStationGui", "TC Crafting Station");
 
         if (craftingTables.size() > 0) {
             logInfo("%d crafting tables registered", craftingTables.size());
