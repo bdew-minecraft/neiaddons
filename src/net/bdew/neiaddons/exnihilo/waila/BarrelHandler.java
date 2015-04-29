e885314c0a519978fa6e89c8ddb9371e6faa5562
bdew <blackdew@gmail.com>
2015-04-29 17:53:40 +0300

    Updated copyright header

--- src/net/bdew/neiaddons/exnihilo/waila/BarrelHandler.java
+++ src/net/bdew/neiaddons/exnihilo/waila/BarrelHandler.java
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) bdew, 2013 - 2014
+ * Copyright (c) bdew, 2013 - 2015
  * https://github.com/bdew/neiaddons
  *
  * This mod is distributed under the terms of the Minecraft Mod Public
  * License 1.0, or MMPL. Please check the contents of the license located in
- * https://raw.github.com/bdew/neiaddons/master/MMPL-1.0.txt
+ * http://bdew.net/minecraft-mod-public-license/
  */
 
 package net.bdew.neiaddons.exnihilo.waila;
--- src/net/bdew/neiaddons/forestry/GeneticsUtils.java
+++ src/net/bdew/neiaddons/forestry/GeneticsUtils.java
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) bdew, 2013 - 2014
+ * Copyright (c) bdew, 2013 - 2015
  * https://github.com/bdew/neiaddons
  *
  * This mod is distributed under the terms of the Minecraft Mod Public
  * License 1.0, or MMPL. Please check the contents of the license located in
- * https://raw.github.com/bdew/neiaddons/master/MMPL-1.0.txt
+ * http://bdew.net/minecraft-mod-public-license/
  */
 
 package net.bdew.neiaddons.forestry;
@@ -112,7 +112,7 @@ public class GeneticsUtils {
                 AddonForestry.instance.logWarning("Template for %s is null, wtf?", species.getUID());
                 return result;
             }
-            ITree tree = (ITree) root.templateAsIndividual(template);
+            ITree tree = root.templateAsIndividual(template);
             for (ItemStack stack : tree.getProduceList()) {
                 if (stack == null || stack.getItem() == null) {
                     AddonForestry.instance.logWarning("%s returned null in produce list", species.getUID());
@@ -136,7 +136,7 @@ public class GeneticsUtils {
                 AddonForestry.instance.logWarning("Template for %s is null, wtf?", species.getUID());
                 return result;
             }
-            ITree tree = (ITree) root.templateAsIndividual(template);
+            ITree tree = root.templateAsIndividual(template);
             for (ItemStack stack : tree.getSpecialtyList()) {
                 if (stack == null || stack.getItem() == null) {
                     AddonForestry.instance.logWarning("%s returned null in specialty list", species.getUID());
